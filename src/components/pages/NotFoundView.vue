ed49a2d5a91ad13cbfd729d421802e2bea9a4fc0
VhArno <arno.vanhee@student.odisee.be>
2024-04-30 09:46:00 +0200

    404 not found page add

---
+++ src/components/pages/NotFoundView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+    <section class="not-found">
+        <h1>Oooooh crap! Page not found.</h1>
+        <p>Try again! Never give up, never back down</p>
+        <img src="/src/assets/imgs/nickeh30.gif" alt="nick Eh 30">
+    </section>
+</template>
+
+<style scoped lang="scss">
+.not-found {
+    margin-top: 2rem;
+    text-align: center;
+
+    img {
+        width: 40%;
+        min-width: 10em;
+    }
+}
+</style>
