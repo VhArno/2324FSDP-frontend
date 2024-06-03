620398819d56651a53a4a7a4c9a8896d6ead8b63
VhArno <arno.vanhee@student.odisee.be>
2024-06-03 15:54:46 +0200

    admin question results css change

--- src/components/organisms/AppAdminResults.vue
+++ src/components/organisms/AppAdminResults.vue
@@ -5,10 +5,11 @@ import { useQuery } from '@tanstack/vue-query'
 import AppLoading from '../atoms/AppLoading.vue'
 import AppChart from '@/components/atoms/AppChart.vue'
 import AppAnswerChart from '@/components/molecules/AppAnswerChart.vue'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useSpecialisationStore } from '@/stores/specialisation'
 import { storeToRefs } from 'pinia'
 import { useDateFormater } from '@/composables/dateFormater'
+import AppButton from '../atoms/AppButton.vue'
 
 const { isPending, isError, data, error } = useQuery({
   queryKey: ['results'],
@@ -25,20 +26,43 @@ const specialistionStore = useSpecialisationStore()
 const { specialisations } = storeToRefs(specialistionStore)
 
 // results chart
-const specializationCount: Map<string, number> = new Map<string, number>()
+const specializationCount = ref<Map<string, number>>(new Map<string, number>())
 
-/* Setting map keys */
-specialisations.value.map((spec) => {
-  specializationCount.set(spec.name, 0)
-})
 
-/* settings map values */
-data.value?.data.data.map((rest) => {
-  if (specializationCount.has(rest.specialisation.name)) {
-    const value = specializationCount.get(rest.specialisation.name) ?? 0
-    specializationCount.set(rest.specialisation.name, value + 1)
-  }
-})
+const populateSpecializationCount = (results: AllResults[]) => {
+  const tempMap = new Map<string, number>()
+  specialisations.value.forEach((spec) => {
+    tempMap.set(spec.name, 0)
+  })
+  results.forEach((result) => {
+    if (tempMap.has(result.specialisation.name)) {
+      const value = tempMap.get(result.specialisation.name) ?? 0
+      tempMap.set(result.specialisation.name, value + 1)
+    }
+  })
+  specializationCount.value = tempMap
+}
+
+watch(
+  () => data.value,
+  (newData) => {
+    if (newData) {
+      populateSpecializationCount(newData.data.data)
+    }
+  },
+  { immediate: true } // Execute the watcher immediately on component mount
+)
+
+// results charts
+const currChart = ref<number>(0)
+
+const nextChart = () => {
+  currChart.value += 1
+}
+
+const previousChart = () => {
+  currChart.value -= 1
+}
 </script>
 
 <template>
@@ -61,28 +85,28 @@ data.value?.data.data.map((rest) => {
     <h2>There are no results yet</h2>
   </div>
 
-  <div
-    class="results"
-    v-if="!isPending && !isError && data?.data.data && data?.data.data?.length > 0"
-  >
-    <h2>All saved results</h2>
-    <div class="result">
-      <p>id</p>
-      <p>name</p>
-      <span>date</span>
-      <span>result</span>
-      <span>email</span>
-    </div>
+  <div class="table-container">
+    <table class="results" v-if="!isPending && !isError && data?.data.data && data?.data.data?.length > 0">
+      <thead>
+        <tr class="table-head">
+          <th>id</th>
+          <th>name</th>
+          <th>date</th>
+          <th>result</th>
+          <th>user email</th>
+        </tr>
+      </thead>
 
-    <div class="result" v-for="result in data?.data.data" :key="result.id">
-      <p>{{ result.id }}</p>
-      <p>{{ result.name }}</p>
-      <span>{{ useDateFormater(result.created_at).newDate }}</span>
-      <span
-        ><b>{{ result.specialisation.name }} {{ result.specialisation.description }}</b></span
-      >
-      <span>{{ result.user.email }}</span>
-    </div>
+      <tbody>
+        <tr class="result" v-for="result in data?.data.data" :key="result.id">
+          <td>{{ result.id }}</td>
+          <td>{{ result.name }}</td>
+          <td>{{ useDateFormater(result.created_at).newDate }}</td>
+          <td><b>{{ result.specialisation.name }} {{ result.specialisation.description }}</b></td>
+          <td>{{ result.user.email }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 
   <div
@@ -99,34 +123,72 @@ data.value?.data.data.map((rest) => {
       ></AppChart>
     </div>
 
-    <h3>Results per question</h3>
+    <div class="answer-chart-title">
+      <h3>Results per question</h3>
+      <p>{{ currChart+1 }}/{{ userAnswers.data.value?.data.data.length }}</p>
+    </div>
     <div class="answers">
       <div class="answer-charts">
         <AppAnswerChart
           class="chart"
-          v-for="question in userAnswers.data.value?.data.data"
+          v-show="currChart === index"
+          v-for="(question, index) in userAnswers.data.value?.data.data"
           :key="question.id"
           :question="question.question"
           :answers="question.answers"
         ></AppAnswerChart>
+      </div>
+
+      <div class="chart-btns">
+        <AppButton class="chart-btn previous" v-if="userAnswers.data.value" :disabled="currChart <= 0" @click="previousChart()"><i class="fa-solid fa-chevron-left"></i></AppButton>
+        <AppButton class="chart-btn next" v-if="userAnswers.data.value" :disabled="currChart >= userAnswers.data.value?.data.data.length-1" @click="nextChart()"><i class="fa-solid fa-chevron-right"></i></AppButton>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.results {
-  display: flex;
-  flex-flow: column;
-  gap: 1rem;
+.table-container {
+  width: 100%;
+  overflow-x: auto;
 
-  .result {
-    background-color: var(--main-light);
-    padding: 1rem;
-    border-radius: 10px;
-    display: flex;
-    flex-flow: row;
-    justify-content: space-between;
+  .results {
+    width: 100%;
+    min-width: max-content;
+    text-align: center;
+    border-collapse: collapse;
+
+    tr {
+        height: 2.5em;
+        border: 1px solid var(--main-light);
+
+        th {
+          border-right: 1px solid var(--bg);
+        }
+      }
+
+      thead {
+        background-color: var(--main-light);
+
+        th {
+          text-transform: uppercase;
+        }
+      }
+
+      tbody {
+        position: relative;
+
+        tr {
+          .options {
+            padding: 0.5rem;
+
+            &:hover {
+              background-color: var(--main-light);
+              cursor: pointer;
+            }
+          }
+        }
+      }
   }
 }
 
@@ -142,30 +204,50 @@ data.value?.data.data.map((rest) => {
     text-align: center;
     text-decoration: underline;
     font-weight: 600;
+    font-size: 1.2em;
   }
 
   .result-chart {
     text-align: center;
     margin: 0 auto;
     width: 100%;
-    max-width: 50rem;
+    max-width: 60rem;
+  }
+
+  .answer-chart-title {
+    text-align: center;
   }
 
   .answers {
+    display: flex;
+    flex-flow: column;
+    gap: 2rem;
     text-align: center;
     margin-top: 1rem;
-    overflow-x: scroll;
     width: 100%;
+    align-items: center;
+    justify-content: center;
+
+    .chart-btns {
+      display:flex;
+      flex-flow: row;
+      gap: 1rem;
+    }
+
+    .chart-btn {
+      &:disabled{
+        cursor: not-allowed
+      }
+    }
 
     .answer-charts {
-      width: max-content;
-      display: flex;
-      flex-flow: row;
-      gap: 5rem;
+      text-align: center;
+      margin: 0 auto;
+      width: 100%;
+      max-width: 60rem;
 
       .chart {
-        flex: 1;
-        width: 50rem !important;
+        width: 100%;
       }
     }
   }
@@ -177,5 +259,52 @@ data.value?.data.data.map((rest) => {
   justify-content: center;
   align-items: center;
   gap: 1rem;
+}
+
+@media (min-width: 45em) {
+  /*.charts {
+    display: flex;
+    flex-flow: column;
+    gap: 1rem;
+    justify-content: space-between;
+    width: 100%;
+    margin-top: 2rem;
+
+    .answers {
+      display: flex;
+      flex-flow: row;
+      gap: 2rem;
+      text-align: center;
+      margin-top: 1rem;
+      width: 100%;
+      align-items: center;
+      justify-content: center;
+
+      .chart-btns {
+        display:flex;
+        flex-flow: row;
+        gap: 1rem;
+      }
+
+      .previous {
+        order: -1;
+      }
+
+      .next {
+        order: 1;
+      }
+
+      .chart-btn {
+        &:disabled{
+          cursor: not-allowed
+        }
+      }
+
+      .answer-charts {
+        .chart {
+      }
+    }
+  }
+}*/
 }
 </style>
